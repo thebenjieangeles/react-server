ae3e2c6eb33adb8e80004612e090c4a78b7f8d41
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-05-18 21:29:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Note from "./components/Note";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/notes")
+      .then((response) => setNotes(response.data));
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+
+    const noteObject = {
+      content: newNote,
+      important: false,
+      id: notes.length + 1,
+    };
+
+    axios
+      .post("http://localhost:3001/notes", noteObject)
+      .then((response) => {
+        setNotes(notes.concat(response.data));
+        setNewNote("");
+      })
+      .catch((error) => {
+        console.log(error);
+        setNewNote("");
+      });
+  };
+
+  const handleNoteChange = (event) => setNewNote(event.target.value);
+
+  return (
+    <>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>
+            <Note note={note} />
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,5 @@
+function Note({ note }) {
+  return <p>{note.content}</p>;
+}
+
+export default Note;
